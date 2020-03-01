69d3edf44aa8f758269ba85ebc3ac994c69051a5
Leandra Turner <calliet0715@gmail.com>
2020-03-01 12:47:54 -0600

    Comments about each api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,21 @@
-let id = db.length + 1;
+let id = db.length + 1; //Each new note will generate a new id #
 const db = require("./db/db.json");
 
 
-
+//Setup api routes
 module.exports = function (app) {
+
+    //handles api get requests
     app.get("/api/notes", function (req, res) {
+        //will respond with the notes in json form
         res.json(db);
     })
 
-
+    //handles users making/saving
     app.post("/api/notes", function (req, res) {
-        req.body.id = id++;
+
+        req.body.id = id++; //each note will have a unique id#(only part of db.json array, not visible to user)
+        //pushing new notes to the db array
         db.push(req.body)
         fs.writeFile("./db/db.json", JSON.stringify(db), function (err, data) {
             if (err) throw err;
@@ -20,9 +25,11 @@ module.exports = function (app) {
     })
 
     app.delete("/api/notes/:id", function (req, res) {
+        //using the id key to select a particular note and delete it from the db
         var getId = req.params.id;
 
         for (var i = 0; i < db.length; i++)
+            //if the db id equals the getId number, then it will be deleted
             if (db[i].id === parseInt(getId)) {
                 db.splice(i, 1);
             }
@@ -32,4 +39,5 @@ module.exports = function (app) {
         })
         res.json(db);
     })
+
 };
