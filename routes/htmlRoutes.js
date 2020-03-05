f5a6fdbcd67cca1990032fb847da0712d3e1abfe
Orchid23 <calliet0715@gmail.com>
2020-03-05 15:26:17 -0600

    Removal of unnecessary comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,17 +5,15 @@ const path = require("path");
 //HTML requests can be exported and used by the server file
 module.exports = function (app) {
     // HTML GET Requests
-
-    //changed to a slash from (*) b/c html file has homepage as (/)
     
     //renders notes html page
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"))
     })
-    
+
     app.get("*", function (req, res) {
         //renders homepage html page
-        res.sendFile(path.join(__dirname, "../index.html"))
+        res.sendFile(path.join(__dirname, "../public/index.html"))
     })
 
 }
