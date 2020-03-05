5a9b74d69a77b03081495d4c535d50f697e07d30
Orchid23 <calliet0715@gmail.com>
2020-03-05 15:22:54 -0600

    App.get now call home page with asterisk per requirement

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,13 +7,15 @@ module.exports = function (app) {
     // HTML GET Requests
 
     //changed to a slash from (*) b/c html file has homepage as (/)
-    app.get("/", function (req, res) {
-        //renders homepage html page
-        res.sendFile(path.join(__dirname, "../index.html"))
-    })
-
+    
     //renders notes html page
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"))
     })
+    
+    app.get("*", function (req, res) {
+        //renders homepage html page
+        res.sendFile(path.join(__dirname, "../index.html"))
+    })
+
 }
